3020d949bae68fcaeda89b352ec34b4eccf55424
Lina-moon <55811327+Lina-moon@users.noreply.github.com>
2019-11-24 22:33:13 +0300

    Update formula.py

--- formula.py
+++ formula.py
@@ -19,22 +19,33 @@ def csv_reader(file_obj):
     huge = (float(0))
     cost = 0
     for row in reader:
-        smaler = culcarea(variables[1], variables[2], float(row[0]))
+        smaler = culcarea(variables[1], variables[2], float(row[0])*variables[3])
         if smaler > huge:
             huge = smaler
             cost = row[0]
     return cost
 
+
+def WTP_culc(x_1, b_1, a):
+    answer = float(1/(a+x_1*b_1))
+    return answer
+
+
+def Revenue_culc(MS, WTP, Price):
+        revenue = math.exp(MS) * math.exp(WTP) * math.exp(Price)
+        return revenue
+
+
 """ /// Obtaining data from txt file ///  """
 
 indata = open('input.txt', 'r')
-kprice = alpha = betta = float(0);
-variables = [kprice, alpha, betta]
+kprice = alpha = betta = knumber = MS = float(0);
+variables = [kprice, alpha, betta, knumber, MS]
 lines =[]
 for text in indata:
     lines += str(text)
 indata.close
-for i in range(4):
+for i in range(6):
     start = lines.index('[')
     end = lines.index(']')
     j = start + 1
@@ -49,7 +60,7 @@ for i in range(4):
 
 # Check of the variables:
 
-for i in range(3):
+for i in range(5):
     print(variables[i])
 
 # Creation of the CSV file:
@@ -63,4 +74,7 @@ np.savetxt('distribution.csv', a, delimiter=',', fmt='%f')
 csv_path = "distribution.csv"
 with open(csv_path, "r") as f_obj:
     variables[0] = csv_reader(f_obj)
+
 print("Optimal cost is: ", variables[0])
+Price = np.log(variables[3] * float(variables[0]))
+print("Optimized revenue is: ", Revenue_culc(np.log(variables[4]), WTP_culc(Price, variables[2], variables[1]), Price))
